bc351111c10b268b716fb1d34d2e2b5f29df9eb6
Victor Alfonso <impr85@hotmail.com>
2022-10-04 17:18:02 -0500

    first commit

--- src/main/java/com/victor/services/IItemService.java
+++ src/main/java/com/victor/services/IItemService.java
@@ -1,8 +1,6 @@
 package com.victor.services;
 
 import com.victor.dto.ItemDTO;
-import com.victor.dto.ResponseItemsMercadoLibreDTO;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
